8be17bfba73a1ec14d6b53be644532ca0d04391a
Tianyi Xu <xutianyish@gmail.com>
2020-10-28 17:06:58 -0400

    added navbar for users pafe

--- src/components/Navbar/navbar.component.js
+++ src/components/Navbar/navbar.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 import './navbar.css'
-import cookpadIcon from '../images/cookpad.png'
+import cookpadIcon from '../homePage/images/cookpad.png'
 
 export default class Navbar extends Component {
     render(){
--- src/components/adminPage/adminPage.component.js
+++ src/components/adminPage/adminPage.component.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import User from "./user.component"
 import { uid } from "react-uid";
 import './style.css'
+import {Link} from 'react-router-dom'
+import cookpadIcon from '../homePage/images/cookpad.png'
 
 export default class AdminPage extends Component {
     constructor(props){
@@ -18,11 +20,19 @@ export default class AdminPage extends Component {
 
     render(){
         return(
-            <div className="container">       
+            <div className="container-sm">    
+            <div id="navbar">
+                <ul>
+                    <li><img src={cookpadIcon} alt="cookpad" width="50px" height="50px"/><Link to="/homepage/1">COOKPAD</Link></li>
+                    <li><Link to="/">Logout</Link></li>
+                </ul>
+            </div>   
                 <table className="user-list">
                     <tbody>
                         <tr>
                             <th>Username</th>
+                            <th>Ban a user</th>
+                            <th>Promote a user</th>
                         </tr>
                         {this.state.users.map(user => (
                         <User key={uid(user)} /* unique id required to help React render more efficiently when we modify the students list. */
