3bb7a2668d9c70606d4c957e18472d651ba55fc7
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2020-11-03 21:58:41 -0500

    modified adminpage styling

--- src/components/adminPage/userlist/userList.component.js
+++ src/components/adminPage/userlist/userList.component.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { uid } from "react-uid";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell';
 import {Link} from 'react-router-dom';
-import "./style.css"
+import "./userlist.css"
 
 export default class UserList extends Component {
     constructor(props){
@@ -15,29 +16,36 @@ export default class UserList extends Component {
             users : this.props.users
         }
     }
+
+    onViewProfile=(e,uid)=>{
+        e.preventDefault();
+        window.location = "/viewprofile/"+uid+"/"+this.state.uid; 
+    }
     render() {
         return(
             <div className="user-list">
             <Table>
+                <TableHead className="table-head">
+                    <TableRow>
+                        <TableCell align = 'center'>uid</TableCell>
+                        <TableCell align = 'center'>Username</TableCell>
+                        <TableCell align = 'center'>View Profiles</TableCell>
+                        <TableCell align = 'center'>Mange</TableCell>
+                    </TableRow>
+                </TableHead>
                     <TableBody>
-                        <TableRow>
-                            <TableCell>Uid</TableCell>
-                            <TableCell>Username</TableCell>
-                            <TableCell>View Profiles</TableCell>
-                            <TableCell>Mange</TableCell>
-                        </TableRow>
                         {this.state.users.map(user => (
                         <TableRow class="user_row" key={uid(user.username)}>
-                            <TableCell className = "uid">
+                            <TableCell className = "uid" align = 'center'>
                                 {user.uid}
                             </TableCell>
-                            <TableCell className = "username">
+                            <TableCell className = "username" align = 'center'>
                                 {user.username}
                             </TableCell>
-                            <TableCell className = "Profile">
-                                <Link to={"/viewprofile/"+user.uid+"/"+this.state.uid}>View Profile</Link>
+                            <TableCell className = "Profile" align = 'center'>
+                                <button id="view-profile" className="btn btn-outline-primary" onClick={(e)=>this.onViewProfile(e,user.uid)}>View Profile</button>  
                             </TableCell>
-                            <TableCell className = "Operations">
+                            <TableCell className = "Operations" align = 'center'>
                                 <button id = "banButton" className="btn btn-primary" onClick={()=>{}}> Ban </button>
                                 <button id = "promoteButton" className="btn btn-primary" onClick={()=>{}}> Promote </button>
                             </TableCell>
