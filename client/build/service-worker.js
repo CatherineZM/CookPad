406fd752cb18eb9bf54bdf2292521c28a4357b58
Tianyi Xu <xutianyish@gmail.com>
2020-12-03 17:29:06 -0500

    commit to build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e52b4ede0cd6c863a611a70e95b928b.js"
+  "/precache-manifest.3af576acd066d0411a049bfadff38eee.js"
 );
 
 self.addEventListener('message', (event) => {
