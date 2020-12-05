93203f09a0348c0cec740c79ea1a7921000e515b
Tianyi Xu <xutianyish@gmail.com>
2020-12-05 09:32:03 -0500

    setting up mongodb uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@
 
    /* Connnect to our database */
    // Get the URI of the local database, or the one specified on deployment.
-   const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/CookpadAPI'
+   const mongoURI = process.env.MONGODB_URI  || 'mongodb://localhost:27017/CookpadAPI'
    
    mongoose.connect(mongoURI, 
        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
